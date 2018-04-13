a91d898203408ad270894dddfba081d3c9ecc166
demo <Password1>
2018-04-13 15:52:44 +0000

    Updated Xpath identifier for home button #user story #2318

--- src/main/java/LeanFtTest.java
+++ src/main/java/LeanFtTest.java
@@ -223,7 +223,11 @@ public class LeanFtTest extends UnitTestClassBase {
                 .tagName("A").innerText("HEADPHONES ").build()).click();
 
         // Go Home
-        browser.describe(WebElement.class, new XPathDescription("//HEADR[3]/NAVI[1]/DEV[1]/A[@roll=\"link\"][1]/SPAN[1]")).click();
+        //browser.describe(WebElement.class, new XPathDescription("//HEADR[3]/NAVI[1]/DEV[1]/A[@roll=\"link\"][1]/SPAN[1]")).click();
+
+        browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .innerText("dvantage ")
+                .tagName("SPAN").build()).click();
     }
 
 
